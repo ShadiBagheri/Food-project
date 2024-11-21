8122215f5b7381ea8dab7b89216d1476cf6fea9f
shadi bagheri <shadibagheri80@gmail.com>
2024-11-21 21:57:27 +0330

    update categoriesPage filter

--- food/components/templates/CategoriesPage.js
+++ food/components/templates/CategoriesPage.js
@@ -1,21 +1,49 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+
 const CategoriesPage = () => {
+    const router = useRouter();
+    const [query, setQuery] = useState({ difficulty: "", time: "" });
+
+    const changeHandler = (event) => {
+        const { name, value } = event.target;
+        setQuery({...query, [name]: value});
+    }
+
+    const searchHandler = () => {
+        console.log(query);
+        router.push({
+            pathName: "/categories",
+            query,
+        })
+    }
+
     return(
         <div className="flex flex-col w-3/6 mx-auto py-10">
             <h1 className="text-3xl font-bold text-gray-700">Categories</h1>
             <div>
                 <div className="flex my-10 gap-5">
-                    <select className="w-[160px] px-2 text-green-500 focus:border-none shadow-md rounded-lg">
-                        <option>Difficulty</option>
-                        <option>Easy</option>
-                        <option>Medium</option>
-                        <option>Hard</option>
+                    <select className="w-[165px] px-2 text-green-500 focus:border-none shadow-md rounded-lg"
+                            name="difficulty"
+                            value={query.difficulty}
+                            onChange={changeHandler}>
+                        <option value="">Difficulty</option>
+                        <option value="Easy">Easy</option>
+                        <option value="Medium">Medium</option>
+                        <option value="Hard">Hard</option>
                     </select>
-                    <select className="w-[160px] px-2 text-green-500 focus:border-none shadow-md rounded-lg">
-                        <option>Cooking Time</option>
-                        <option>More than 30 min</option>
-                        <option>Less than 30 min</option>
+                    <select className="w-[165px] px-2 text-green-500 focus:border-none shadow-md rounded-lg"
+                            name="time"
+                            value={query.time}
+                            onChange={changeHandler}>
+                        <option value="">Cooking Time</option>
+                        <option value="More">More than 30 min</option>
+                        <option value="Less">Less than 30 min</option>
                     </select>
-                    <button className="py-2 px-3 text-sm text-white font-bold rounded-md bg-green-500 hover:bg-green-600">Search</button>
+                    <button className="py-2 px-3 text-sm text-white font-bold rounded-md bg-green-500 hover:bg-green-600"
+                            onClick={searchHandler}>
+                        Search
+                    </button>
                 </div>
             </div>
         </div>
--- food/pages/categories/index.js
+++ food/pages/categories/index.js
@@ -1,11 +1,46 @@
 import CategoriesPage from "@/components/templates/CategoriesPage";
 
-const Categories = () => {
+const Categories = ({ data }) => {
     return(
         <>
-            <CategoriesPage/>
+            <CategoriesPage data={data}/>
         </>
     )
 }
 
 export default Categories
+
+export async function getServerSideProps(context) {
+    const { query: { difficulty, time } } = context;
+    const res = await fetch("http://localhost:4000/data");
+    const data = await res.json();
+
+    const filteredData = data.filter(item => {
+        const difficultyResult = item.details.filter(detail =>
+            (detail.Difficulty && detail.Difficulty === difficulty)
+        );
+
+        const timeResult = item.details.filter(detail => {
+            const cookingTime = detail["Cooking Time"] || "";
+            const timeDetails = cookingTime.split(" ");
+
+            if (time === "less" && timeDetails && +timeDetails <= 30){
+                return detail;
+            }else if (time === "more" && +timeDetails > 30){
+                return detail;
+            }
+        });
+
+        if (time && difficulty && difficultyResult.length && timeResult.length){
+            return item;
+        }else if (!time && difficulty && difficultyResult.length){
+            return item;
+        }else if (time && !difficulty && timeResult){
+            return item;
+        }
+    });
+    
+    return{
+        props: { data: filteredData }
+    }
+}
